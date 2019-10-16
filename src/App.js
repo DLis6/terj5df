4c35b7e261e1762cb67bfedfd4b2a0de2a75f162
David <54381677+DLis6@users.noreply.github.com>
2019-10-15 20:05:33 -0500

    AdminInvitados

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      vName: "",
+      vLast: "",
+      names: []
+    };
+    this.retornoNombre = this.retornoNombre.bind(this);
+    this.retornoApellido = this.retornoApellido.bind(this);
+    this.action = this.action.bind(this);
+  }
+
+  retornoNombre(name) {
+    this.setState({
+      vName: name
+    });
+  }
+  retornoApellido(lastName) {
+    this.setState({
+      vLast: lastName
+    });
+  }
+  action(e) {
+    e.preventDefault();
+    if (this.state.vName && this.state.vLast) {
+    
+      this.setState({
+        names: this.state.names.concat([
+          {
+            vName: this.state.vName,
+            vLast: this.state.vLast
+          }
+        ]),
+        vLast: "",
+        vName: ""
+      });
+    }
+  }
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-sm-offset-3">
-            <form>
+            <form onSubmit={this.action}>
               <div className="form-group">
                 <label htmlFor="first-name">Nombre</label>
-                <input type="text" className="form-control" name="first-name" />
+                <input
+                  onChange={e => {
+                    this.retornoNombre(e.target.value);
+                  }}
+                  type="text"
+                  className="form-control"
+                  name="first-name"
+                  value={this.state.vName}
+                />
               </div>
 
               <div className="form-group">
                 <label htmlFor="last-name">Apellido</label>
-                <input type="text" className="form-control" name="last-name" />
+                <input
+                  onChange={e => {
+                    this.retornoApellido(e.target.value);
+                  }}
+                  type="text"
+                  className="form-control"
+                  name="last-name"
+                  value={this.state.vLast}
+                />
               </div>
 
               <div className="action">
-                <button type="submit" className="btn btn-primary">Agregar Invitado</button>
+                <button
+                  type="submit"
+                  className="btn btn-primary"
+                >
+                  Agregar Invitado
+                </button>
               </div>
             </form>
 
@@ -30,16 +90,21 @@ class App extends Component {
                 </tr>
               </thead>
               <tbody>
-
+                {this.state.names.map((element,index) => {
+                  return (
+                    <tr key={`element_${element.vName}_${index}`}>
+                      <td>{element.vName}</td>
+                      <td>{element.vLast}</td>
+                    </tr>
+                  );
+                })}
               </tbody>
             </table>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
-
-
+export default App;
